c59a1c27301d69b3f223ba279f47934ceb43d937
ayush.779gupta@gmail.com <ayush.779gupta@gmail.com>
2025-09-26 14:02:34 +0000

    Cloning prophecy project main branch

---
+++ niuhniu/pipelines/report_top_customers/code/job/graph/Report.py
@@ -0,0 +1,19 @@
+from pyspark.sql import *
+from pyspark.sql.functions import *
+from pyspark.sql.types import *
+from job.config.ConfigStore import *
+from job.udfs.UDFs import *
+
+def Report(spark: SparkSession, in0: DataFrame):
+    if Config.fabricName == "dev":
+        in0.write\
+            .option("header", True)\
+            .option("sep", ",")\
+            .option("ignoreLeadingWhiteSpace", True)\
+            .option("ignoreTrailingWhiteSpace", True)\
+            .mode("overwrite")\
+            .option("separator", ",")\
+            .option("header", True)\
+            .csv("dbfs:/Prophecy/a561c4f9c3cc2b29f13c891b1dc1b5d3/report-top-customers.csv")
+    else:
+        raise Exception("No valid dataset present to read fabric")
