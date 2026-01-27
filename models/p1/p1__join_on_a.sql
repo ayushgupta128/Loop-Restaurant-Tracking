da042ae60a17502d43b3069962b1a19b2d11283d
ayush.779gupta@gmail.com <ayush.779gupta@gmail.com>
2026-01-27 07:06:45 +0000

    AI: joined limit_gem and numeric_metrics_1_1 on column a

--- models/p1/p1__join_on_a.sql
+++ models/p1/p1__join_on_a.sql
@@ -6,7 +6,15 @@
   })
 }}
 
-WITH numeric_metrics_1 AS (
+WITH numeric_metrics_1_1 AS (
+
+  SELECT * 
+  
+  FROM {{ ref('p1')}}
+
+),
+
+numeric_metrics_1 AS (
 
   SELECT * 
   
@@ -44,8 +52,23 @@ limit_gem AS (
   
   LIMIT 1
 
+),
+
+join_on_a AS (
+
+  SELECT 
+    limit_gem.a AS A,
+    limit_gem.b AS B,
+    limit_gem.c AS C,
+    numeric_metrics_1_1.b AS N_B,
+    numeric_metrics_1_1.c AS N_C
+  
+  FROM limit_gem
+  INNER JOIN numeric_metrics_1_1
+     ON limit_gem.a = numeric_metrics_1_1.a
+
 )
 
 SELECT *
 
-FROM limit_gem
+FROM join_on_a
