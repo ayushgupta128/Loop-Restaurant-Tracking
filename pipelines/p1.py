f24b4e44466f12e9c307a5f6de2339eec4888076
ayush.779gupta@gmail.com <ayush.779gupta@gmail.com>
2026-01-27 07:06:35 +0000

    Add `numeric_metrics_1_1` dataset to `p1` pipeline

--- pipelines/p1.py
+++ pipelines/p1.py
@@ -11,6 +11,12 @@ with DAG():
         table = {"name" : "p1", "sourceType" : "Seed"}, 
         writeOptions = {"writeMode" : "overwrite"}
     )
+    numeric_metrics_1_1 = Task(
+        task_id = "numeric_metrics_1_1", 
+        component = "Dataset", 
+        table = {"name" : "p1", "sourceType" : "Seed"}, 
+        writeOptions = {"writeMode" : "overwrite"}
+    )
     p1__limit_gem = Task(task_id = "p1__limit_gem", component = "Model", modelName = "p1__limit_gem")
     numeric_metrics.out >> p1__limit_gem.in_0
     numeric_metrics_1.out >> p1__limit_gem.in_1
