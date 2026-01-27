fecfa26fe46c9fb0eeed6cbdd18832032b258828
ayush.779gupta@gmail.com <ayush.779gupta@gmail.com>
2026-01-27 07:18:13 +0000

    AI: joined limit_gem and numeric_metrics_1_1 on column a

--- pipelines/p1.py
+++ pipelines/p1.py
@@ -5,18 +5,19 @@ with DAG():
         table = {"name" : "p1", "sourceType" : "Seed"}, 
         writeOptions = {"writeMode" : "overwrite"}
     )
-    numeric_metrics = Task(
-        task_id = "numeric_metrics", 
-        component = "Dataset", 
-        table = {"name" : "p1", "sourceType" : "Seed"}, 
-        writeOptions = {"writeMode" : "overwrite"}
-    )
     numeric_metrics_1_1 = Task(
         task_id = "numeric_metrics_1_1", 
         component = "Dataset", 
         table = {"name" : "p1", "sourceType" : "Seed"}, 
         writeOptions = {"writeMode" : "overwrite"}
     )
-    p1__limit_gem = Task(task_id = "p1__limit_gem", component = "Model", modelName = "p1__limit_gem")
-    numeric_metrics.out >> p1__limit_gem.in_0
-    numeric_metrics_1.out >> p1__limit_gem.in_1
+    p1__join_on_a = Task(task_id = "p1__join_on_a", component = "Model", modelName = "p1__join_on_a")
+    numeric_metrics = Task(
+        task_id = "numeric_metrics", 
+        component = "Dataset", 
+        table = {"name" : "p1", "sourceType" : "Seed"}, 
+        writeOptions = {"writeMode" : "overwrite"}
+    )
+    numeric_metrics.out >> p1__join_on_a.in_0
+    numeric_metrics_1.out >> p1__join_on_a.in_1
+    numeric_metrics_1_1.out >> p1__join_on_a.in_3
