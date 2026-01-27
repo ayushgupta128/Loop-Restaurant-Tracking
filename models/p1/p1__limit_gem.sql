3ade9f4aafcba9134b57687fda72f14085f299d4
ayush.779gupta@gmail.com <ayush.779gupta@gmail.com>
2026-01-27 07:18:00 +0000

    Replace `join_on_a` transformation with `limit_gem` in p1 pipeline and update dependencies

--- models/p1/p1__limit_gem.sql
+++ models/p1/p1__limit_gem.sql
@@ -6,15 +6,7 @@
   })
 }}
 
-WITH numeric_metrics_1_1 AS (
-
-  SELECT * 
-  
-  FROM {{ ref('p1')}}
-
-),
-
-numeric_metrics_1 AS (
+WITH numeric_metrics_1 AS (
 
   SELECT * 
   
@@ -52,23 +44,8 @@ limit_gem AS (
   
   LIMIT 1
 
-),
-
-join_on_a AS (
-
-  SELECT 
-    limit_gem.a AS A,
-    limit_gem.b AS B,
-    limit_gem.c AS C,
-    numeric_metrics_1_1.b AS N_B,
-    numeric_metrics_1_1.c AS N_C
-  
-  FROM limit_gem
-  INNER JOIN numeric_metrics_1_1
-     ON limit_gem.a = numeric_metrics_1_1.a
-
 )
 
 SELECT *
 
-FROM join_on_a
+FROM limit_gem
