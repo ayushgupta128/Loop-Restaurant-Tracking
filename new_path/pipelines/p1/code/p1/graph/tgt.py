8d00afcabf22cb9b9dd930b7b68be91c7821b8e7
ayush.779gupta@gmail.com <ayush.779gupta@gmail.com>
2026-02-20 12:14:04 +0000

    Update `tgt` transformation to write output to `final_table_test` in `p1` pipeline

--- new_path/pipelines/p1/code/p1/graph/tgt.py
+++ new_path/pipelines/p1/code/p1/graph/tgt.py
@@ -7,4 +7,4 @@ from p1.config.ConfigStore import *
 from p1.functions import *
 
 def tgt(spark: SparkSession, in0: DataFrame):
-    in0.write.format("delta").mode("overwrite").saveAsTable("`ayush_demos`.`demos`.`final_table`")
+    in0.write.format("delta").mode("overwrite").saveAsTable("`ayush_demos`.`demos`.`final_table_test`")
