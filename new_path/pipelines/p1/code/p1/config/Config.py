f6da53534d9614fc299ef5e59716b4c32f6c5b90
ayush.779gupta@gmail.com <ayush.779gupta@gmail.com>
2026-03-17 12:42:10 +0000

    Add new p1 configuration and refactor config handling, plus config metadata
    - Introduce new vrefq.json resource and corresponding updated Config.py, ConfigStore.py
    - Add project configuration metadata and default config files under projectConfigurations
    - Add pbt_aspects.yml for configuration aspects
    - Track latest workflow and config state files for pipeline p1 in new_path layout

--- new_path/pipelines/p1/code/p1/config/Config.py
+++ new_path/pipelines/p1/code/p1/config/Config.py
@@ -3,11 +3,13 @@ from prophecy.config import ConfigBase
 
 class Config(ConfigBase):
 
-    def __init__(self, vrfe: str=None, **kwargs):
+    def __init__(self, vrfe: str=None, prophecy_project_config=None, **kwargs):
         self.spark = None
-        self.update(vrfe)
+        self.update(vrfe, prophecy_project_config, **kwargs)
 
-    def update(self, vrfe: str="vera", **kwargs):
+    def update(self, vrfe: str="vera", prophecy_project_config=None, **kwargs):
         prophecy_spark = self.spark
+        prophecy_project_config = self.update_project_conf_values(prophecy_project_config, kwargs)
+        self.update_and_add_project_config(prophecy_spark, prophecy_project_config)
         self.vrfe = vrfe
         pass
